1df5f1ecbd4a86c2343d21a74cf86d6850c554e7
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-09-19 14:01:13 +0530

    Created SQL statements for data updation

--- database/update-statements.sql
+++ database/update-statements.sql
@@ -1,1 +1,47 @@
 use tasks_app;
+
+
+-- updating user password
+update users
+set user_password = "new password"
+where user_id = "existing user id";
+
+-- updating user email
+update users
+set user_email = "new email"
+where user_id = "existing user id";
+
+
+-- updating task details
+
+-- the backend logic will first get existing details,
+-- then the new details given by user will be inserted here.
+-- rest will be old details again.
+update tasks
+set 
+	task_name = "new task name",
+	task_description = "new task description",
+    due_date = "new due date",
+    has_sub_tasks = "if it has sub tasks"
+where task_id = "specific task id";
+
+-- same logic with sub tasks
+update sub_tasks
+set
+	sub_task_name = "new sub task name",
+    due_date = "new due date"
+where sub_task_id = "specific sub task id";
+
+
+-- more or less the same logic with updating tags too
+update tags
+set
+	tag_name = "new tag name",
+    tag_colour = "new tag colour"
+where tag_id = "specific tag id";
+
+
+-- no need to update the tasks-tags junction table
+-- as changing tags or tasks here
+-- essentially means removing a tag from a task.
+-- so using DELETE will be better.
