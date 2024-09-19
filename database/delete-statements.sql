ab34adcbbedda7284ae8ddb0094c8efec7625692
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-09-19 15:33:49 +0530

    Created SQL statements for deletion

--- database/delete-statements.sql
+++ database/delete-statements.sql
@@ -1,1 +1,27 @@
 use tasks_app;
+
+-- each table get's their own delete statement
+-- in the backend logic, i'll call them whenever necessary
+
+-- deleting user
+delete from users
+where user_id = "given user id";
+
+-- deleting a task
+delete from tasks
+where task_id = "given task id";
+
+-- deleting a sub task
+delete from sub_tasks
+where sub_task_id = "given sub task id";
+
+-- deleting a tag
+delete from tags
+where tag_id = "given tag id";
+
+-- removing a tag from a task
+-- which means have to remove link in the junction table
+delete from tasks_tags
+where
+	tag_id = "given tag id"
+    and task_id = "given task id";
