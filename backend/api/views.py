f7f0752e02f777671a42783ffc52b8c152f8f6b0
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-09-30 21:44:30 +0530

    Added API for changing password successfully!

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 # from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer, ChangePasswordSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
@@ -29,6 +32,26 @@ class GetUserView(generics.RetrieveAPIView):
         queryset = self.get_queryset()
         serializer = UserSerializer(queryset.get(username=username))
         return Response(serializer.data)
+
+
+class UpdatePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        user_obj = request.user
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            old_password = serializer.data.get("old_password")
+            if not user_obj.check_password(old_password):
+                return Response(
+                    {"old password": ["Wrong password"]},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            user_obj.set_password(serializer.data.get("new_password"))
+            user_obj.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # NOTE: print(User.check_password(request.user, "thisiswrongpass"))
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,19 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from api.views import CreateUserView, GetUserView
+from api.views import CreateUserView, GetUserView, UpdatePasswordView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # URL for registering user
     path("api/user/register/", CreateUserView.as_view(), name="Register"),
     # URL for getting user details
-    path("api/user/get/<int:pk>/", GetUserView.as_view(), name="Get User"),
     path("api/user/get/", GetUserView.as_view(), name="Get User"),
+    # URL for changing password
+    path(
+        "api/user/change-password/",
+        UpdatePasswordView.as_view(),
+        name="Change-password",
+    ),
     # Below URLs are for SimpleJWT so it can generate tokens and refresh tokens
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # this enables auth stuff in the rest_framework ig
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
-
