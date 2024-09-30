9e94cea7bda419b84655227d0f83b6bc114e96d5
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-09-30 18:40:15 +0530

    Created View for creating a new user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
+
+
+# This declared the CREATE API
+class CreateUserView(generics.CreateAPIView):
+  # gives list of all users in the database to check if new user already exists
+  queryset = User.objects.all() 
+
+  serializer_class = UserSerializer
+
+  # allows anyone to use this API since we want anyone to create an account
+  permission_classes = [AllowAny] 
