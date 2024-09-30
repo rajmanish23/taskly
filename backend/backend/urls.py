48c1c8be95dacaa71df7a574f3eb6df55badc03f
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-09-30 19:11:01 +0530

    got new formatter bois

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path(
         "api/user/get/<int:pk>/", GetUserView.as_view(), name="Get User"
     ),  # URL for getting user details
-    
     # Below URLs are for SimpleJWT so it can generate tokens and refresh tokens
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
@@ -20,3 +19,4 @@ urlpatterns = [
         "api-auth/", include("rest_framework.urls", namespace="rest_framework")
     ),  # this enables auth stuff in the rest_framework ig
 ]
+
