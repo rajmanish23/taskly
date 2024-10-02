408c14d914d86c0e983171ee15b130a7d1ef3a84
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-10-02 15:01:17 +0530

    Registered models with admin panel so that they can show up on the admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SubTask, Tag, Task
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Tag)
