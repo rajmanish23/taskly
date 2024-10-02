eea942ce2ea19f14d60dcdfe38c8a95cccf545f5
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-10-02 15:11:25 +0530

    Added many-to-many relation between tags and tasks + formatted code

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,27 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     due_at = models.DateTimeField(blank=False, null=False)
     has_sub_tasks = models.BooleanField(default=False)
-    # related_name gives a name from User obj to get this Task data. So it will be user.tasks here
+
+    # related_name gives a name from User obj to get this Task data.
+    # So it will be user.tasks here
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
 
     def __str__(self) -> str:
         return self.name
-    
+
 
 class SubTask(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
     # A Sub task may or may not have a due date
-    # maybe user want to just have a bunch of sub tasks under one main task 
+    # maybe user want to just have a bunch of sub tasks under one main task
     # and they don't want to give any due dates for each sub tasks
     due_at = models.DateTimeField()
-    parent_task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="sub_tasks")
+
+    parent_task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name="sub_tasks"
+    )
 
     def __str__(self) -> str:
         return self.name
@@ -31,6 +38,9 @@ class Tag(models.Model):
     name = models.CharField(max_length=20, blank=False, null=False)
     color_hex = models.CharField(max_length=7, blank=False, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tags")
+
+    # defines the many-to-many relationship between tags and tasks
+    tasks = models.ManyToManyField(Task, blank=True)
 
     def __str__(self) -> str:
         return self.name
