62b2d661ba807ebcde797d05b8884e5aef57fff0
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-10-07 14:30:57 +0530

    Reordered views

--- backend/api/views.py
+++ backend/api/views.py
@@ -18,6 +18,49 @@ from .serializers import (
 )
 
 
+# This declared the CREATE API for USER
+class CreateUserView(generics.CreateAPIView):
+    # gives list of all users in the database to check if new user already exists
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # allows anyone to use this API since we want anyone to create an account
+    permission_classes = [AllowAny]
+
+
+# This is for GET API for USER
+class GetUserView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        username = request.user.get_username()
+        queryset = self.get_queryset()
+        serializer = UserSerializer(queryset.get(username=username))
+        return Response(serializer.data)
+
+
+# Change password for USER
+class UpdatePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user_obj = request.user
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            old_password = serializer.data.get("old_password")
+            if not user_obj.check_password(old_password):
+                return Response(
+                    {"old password": ["Wrong password"]},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            user_obj.set_password(serializer.data.get("new_password"))
+            user_obj.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class TaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
@@ -80,49 +123,6 @@ class TaskUpcomingListView(generics.ListAPIView):
             due_at__gt=filter_datetime
         )
         return queryset
-
-
-# This declared the CREATE API for USER
-class CreateUserView(generics.CreateAPIView):
-    # gives list of all users in the database to check if new user already exists
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    # allows anyone to use this API since we want anyone to create an account
-    permission_classes = [AllowAny]
-
-
-# This is for GET API for USER
-class GetUserView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        username = request.user.get_username()
-        queryset = self.get_queryset()
-        serializer = UserSerializer(queryset.get(username=username))
-        return Response(serializer.data)
-
-
-# Change password for USER
-class UpdatePasswordView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def put(self, request):
-        user_obj = request.user
-        serializer = ChangePasswordSerializer(data=request.data)
-        if serializer.is_valid():
-            old_password = serializer.data.get("old_password")
-            if not user_obj.check_password(old_password):
-                return Response(
-                    {"old password": ["Wrong password"]},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            user_obj.set_password(serializer.data.get("new_password"))
-            user_obj.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TagListCreateView(generics.ListCreateAPIView):
