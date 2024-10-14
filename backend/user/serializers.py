04ef7f5b713b12b5ac044585287d812996227a26
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-10-14 15:04:51 +0530

    Replaced "username" field with "email" field to reflect the custom model

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "password", "first_name", "last_name"]
+        fields = ["id", "email", "password", "first_name", "last_name"]
 
         # This provides additional functionality
         # Here, making first and last name required
