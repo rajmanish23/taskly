ee933620dc67f25f90a2632c6960a3deb127504e
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-10-14 14:58:04 +0530

    Enabled Serializers, urls and views in the user app

--- backend/user/views.py
+++ backend/user/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# from .models import User
+from .models import User
 from .serializers import (
     UserSerializer,
     ChangePasswordSerializer
