0c42a87854b670d263e4d2ab3c561406b2ac50ed
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-10-14 14:57:16 +0530

    Few changes for the serializer in api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,10 @@
 import datetime
 
-from django.contrib.auth.models import User
-
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Task, Tag
 from .serializers import (
-    # UserSerializer,
-    # ChangePasswordSerializer,
     TaskSerializer,
     TagSerializer,
     SubTaskSerializer,
