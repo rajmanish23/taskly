80c17de0d2a410e17bf3a3a3c2e0fd2b55ad3609
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-09 13:59:53 +0530

    Added an interceptor which adds the access tokens to every request sent

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,22 @@
-// import axios from "axios";
-// import { ACCESS_KEY } from "./constants";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { ACCESS_KEY } from "./constants";
 
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_BASE_API_URL
-// })
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_API_URL
+})
+
+api.interceptors.request.use(
+    (config) => {
+        const token = Cookies.get(ACCESS_KEY)
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default api
