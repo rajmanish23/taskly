e62e996ab8de1bce057bdc54ac3c0dab2c1fcfaa
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-09 14:49:43 +0530

    Added paths for a few pages. Really just to test out the ProtectedRoute comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,41 @@
+import { Route, Routes } from "react-router-dom";
+import Cookies from 'js-cookie'
+
+import { ACCESS_KEY, REFRESH_KEY } from "./constants";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Today from "./pages/Today";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./pages/NotFound";
+
+// function Logout() {
+//   Cookies.remove(ACCESS_KEY)
+//   Cookies.remove(REFRESH_KEY)
+//   return <Navigate to="/login" />
+// }
+
+function RegisterAndLogout() {
+  Cookies.remove(ACCESS_KEY);
+  Cookies.remove(REFRESH_KEY);
+  return <Register />
+}
+
 function App() {
   return (
     <div>
-        
+      <Routes>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Today />
+          </ProtectedRoute>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
