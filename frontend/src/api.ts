6b0ee4bdea206c479bd772692141eee025b1511c
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-10 19:18:45 +0530

    Reformatting

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,20 +3,20 @@ import Cookies from "js-cookie";
 import { ACCESS_KEY } from "./constants";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_API_URL
-})
+  baseURL: import.meta.env.VITE_BASE_API_URL,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = Cookies.get(ACCESS_KEY)
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = Cookies.get(ACCESS_KEY);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter
       future={{
         v7_startTransition: true,
-        v7_relativeSplatPath: true
+        v7_relativeSplatPath: true,
       }}
     >
       <App />
