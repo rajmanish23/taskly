3e4a83147beffe8c24829c97ffbd7addf6486115
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-14 11:55:31 +0530

    Added .env const for get tags list api url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -5,6 +5,7 @@ export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 export const LOGIN_API_URL = import.meta.env.VITE_LOGIN_API_URL;
 export const TOKEN_REFRESH_API_URL = import.meta.env.VITE_TOKEN_REFRESH_URL;
 export const REGISTER_API_URL = import.meta.env.VITE_REGISTER_API_URL;
+export const GET_TAGS_LIST_API_URL = import.meta.env.VITE_GET_TAGS_LIST_API_URL;
 
 export const LOGIN_PAGE_URL = import.meta.env.VITE_LOGIN_PAGE_URL;
 export const REGISTER_PAGE_URL = import.meta.env.VITE_REGISTER_PAGE_URL;
