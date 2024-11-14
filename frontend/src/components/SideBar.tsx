5d84a7d90ce927d4a493555760f42d747bb5d0d7
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-14 11:58:01 +0530

    Wrote a barebones code for sidebar component

---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useState } from "react";
+
+import logoImg from "../assets/taskly-logo-big.png";
+import profileImg from "../assets/default-profile.jpg";
+import { getTagsAPI } from "../API/TagsAPI";
+
+type SideBarProps = {
+  mode: "SETTINGS" | "NORMAL";
+};
+
+const SideBar = ({ mode }: SideBarProps) => {
+  const [tags, setTags] = useState<Tag[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const getTags = useCallback(async () => {
+    if (mode === "SETTINGS") return;
+    setIsLoading(true);
+    try {
+      const data = await getTagsAPI();
+      setTags(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [mode]);
+
+  useEffect(() => {
+    getTags().catch((e) => console.log(e));
+  }, [getTags]);
+
+  return (
+    <div>
+      <img src={logoImg} />
+      {mode === "NORMAL" ? (
+        <>
+          <div>
+            <p>Views</p>
+            <button>Today</button>
+            <button>Upcoming</button>
+          </div>
+          <div>
+            <p>Tags</p>
+            {isLoading ? (
+              <>Loading...</>
+            ) : (
+              <>
+                {tags?.map((each) => (
+                  <button key={each.sId}>{each.name}</button>
+                ))}
+              </>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <div>Settings</div>
+          <button>Edit Name</button>
+          <button>Edit Email</button>
+          <button>Change Password</button>
+          <button>Logout</button>
+          <button>Delete Account</button>
+        </>
+      )}
+      <div>
+        <img src={profileImg} />
+        <p>User name here</p>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
--- frontend/src/pages/Today.tsx
+++ frontend/src/pages/Today.tsx
@@ -1,9 +1,15 @@
+import SideBar from "../components/SideBar";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 
 const Today = () => {
   useDocumentTitle("Today");
 
-  return <div>Today</div>;
+  return (
+    <>
+      <div>Today</div>
+      <SideBar mode="NORMAL" />
+    </>
+);
 };
 
 export default Today;
