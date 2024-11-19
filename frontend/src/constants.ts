238acd78ccb5c5fb7a709f17f0ccdab133c9f492
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-19 09:09:22 +0530

    Moved API and page URLs into the constants file instead of relying on .env file

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -7,17 +7,17 @@ export const STYLE_TEXT_COLOR = "#ec8e8e";
 
 export const DEVICE_WIDTH = {
   MOBILE: "(max-width: 767px)",
-  PC: "(min-width: 768px)"
-}
+  PC: "(min-width: 768px)",
+};
 
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
-export const LOGIN_API_URL = import.meta.env.VITE_LOGIN_API_URL;
-export const TOKEN_REFRESH_API_URL = import.meta.env.VITE_TOKEN_REFRESH_URL;
-export const REGISTER_API_URL = import.meta.env.VITE_REGISTER_API_URL;
-export const GET_TAGS_LIST_API_URL = import.meta.env.VITE_GET_TAGS_LIST_API_URL;
-export const GET_USER_API_URL = import.meta.env.VITE_GET_USER_API_URL;
+export const LOGIN_API_URL = "/api/token/";
+export const TOKEN_REFRESH_API_URL = "/api/token/refresh/";
+export const REGISTER_API_URL = "/api/user/register/";
+export const GET_TAGS_LIST_API_URL = "/api/tags/";
+export const GET_USER_API_URL = "/api/user/get/";
 
-export const LOGIN_PAGE_URL = import.meta.env.VITE_LOGIN_PAGE_URL;
-export const REGISTER_PAGE_URL = import.meta.env.VITE_REGISTER_PAGE_URL;
-export const TODAY_PAGE_URL = import.meta.env.VITE_TODAY_PAGE_URL;
-export const UPCOMING_PAGE_URL = import.meta.env.VITE_UPCOMING_PAGE_URL;
+export const LOGIN_PAGE_URL = "/login";
+export const REGISTER_PAGE_URL = "/register";
+export const TODAY_PAGE_URL = "/";
+export const UPCOMING_PAGE_URL = "/upcoming";
