acb3cec23b4f5f7dec3bb559654acbae466a78e9
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-19 14:25:01 +0530

    Currently working on fully implementing the today view. So far, succesfully added the API getter for /today

--- frontend/src/pages/Today.tsx
+++ frontend/src/pages/Today.tsx
@@ -1,9 +1,36 @@
 // import SideBar from "../components/SideBar";
+import { useCallback, useEffect, useState } from "react";
+import { getTodayTasksAPI } from "../API/tasksAPI";
 import TasksListView from "../components/TasksListView";
 import useDocumentTitle from "../hooks/useDocumentTitle";
+import { isAPIErrorMessage } from "../utils/objectTypeCheckers";
 
 const Today = () => {
   useDocumentTitle("Today");
+
+  const [tasks, setTasks] = useState<Task[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const getTasks = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const data = await getTodayTasksAPI();
+      if (isAPIErrorMessage(data)) {
+        setTasks([])
+        console.log(data.detail)
+      } else {
+        setTasks(data)
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getTasks().catch((e) => console.log(e));
+  }, [getTasks]);
 
   return (
     <>
