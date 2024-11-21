05cdf51ac2219f9420f307a26a9dc0a911ccaeb7
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-21 17:22:57 +0530

    Removed the styles that was keeping the main content view thing beside the side bar at the center of the page

--- frontend/src/components/MainView/styles.ts
+++ frontend/src/components/MainView/styles.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const SC_MainViewContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: row;
   height: 100%;
 `;
