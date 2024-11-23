8e86ba907b9b832b4f0acc24210611a33011d122
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-23 13:39:50 +0530

    Added code that will automatically refresh the tokens if the access token is expired when the API is called

--- frontend/src/API/baseAPI.ts
+++ frontend/src/API/baseAPI.ts
@@ -3,6 +3,12 @@ import Cookies from "js-cookie";
 import { decamelizeKeys } from "humps";
 
 import { ACCESS_KEY, BASE_API_URL } from "../constants";
+import {
+  areTokensValid,
+  isAccessTokenExpired,
+  isRefreshTokenExpired,
+} from "../utils/tokenValidator";
+import { refreshSession } from "./isAuthorizedAPI";
 
 const baseAPI = axios.create({
   baseURL: BASE_API_URL,
@@ -11,11 +17,18 @@ const baseAPI = axios.create({
       headers["Content-Type"] = "application/json";
       return JSON.stringify(decamelizeKeys(data));
     },
-  ]
+  ],
 });
 
 baseAPI.interceptors.request.use(
-  (config) => {
+  async (config) => {
+    if (!areTokensValid()) throw Error("Tokens are not valid");
+    if (isAccessTokenExpired()) {
+      if (isRefreshTokenExpired()) throw Error("Refresh token is expired");
+      // The function below set's the token as the cookie by itself so just waiting for this to set
+      // and then trying to access the token should return the new access token.
+      await refreshSession();
+    }
     const token = Cookies.get(ACCESS_KEY);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
