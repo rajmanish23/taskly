f2e4909e0caeaecb871eb9004b2f80de04eb24ee
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-24 15:48:27 +0530

    Added the header and a spinner to indicate it is loading along with an error message in case the fetched user data is undefined somehow

--- frontend/src/components/SettingsProfileView/index.tsx
+++ frontend/src/components/SettingsProfileView/index.tsx
@@ -1,5 +1,20 @@
 import { useEffect, useState, useCallback } from "react";
+import { BarLoader } from "react-spinners";
+
+import profileImg from "../../assets/default-profile.jpg";
+
 import { getUserAPI } from "../../API/userAPI";
+import {
+  SC_BackgroundContainer,
+  SC_CentralNoDataContainer,
+} from "../commonStyles";
+import ViewHeader from "../ViewHeader";
+import {
+  BAR_LOADER_HEIGHT,
+  BAR_LOADER_WIDTH,
+  STYLE_TEXT_COLOR,
+} from "../../constants";
+import ErrorMessage from "../ErrorMessage";
 
 const SettingsProfileView = () => {
   const [userDetails, setUserDetails] = useState<User>();
@@ -21,7 +36,35 @@ const SettingsProfileView = () => {
     getUserDetails().catch((e) => console.log(e));
   }, [getUserDetails]);
 
-  return <div>SettingsProfileView</div>;
+  return (
+    <SC_BackgroundContainer>
+      <ViewHeader h1Text="Profile" />
+      {isLoading ? (
+        <SC_CentralNoDataContainer>
+          <BarLoader
+            color={STYLE_TEXT_COLOR}
+            height={BAR_LOADER_HEIGHT}
+            width={BAR_LOADER_WIDTH}
+          />
+        </SC_CentralNoDataContainer>
+      ) : userDetails === undefined ? (
+        <SC_CentralNoDataContainer>
+          <ErrorMessage errorMessage="We could not fetch your profile details. Please try again later." />
+        </SC_CentralNoDataContainer>
+      ) : (
+        <div>
+          <div>
+            <img src={profileImg} />
+            <h1>{userDetails.fullName}</h1>
+          </div>
+          <div>
+            <p>Email</p>
+            <p>{userDetails.email}</p>
+          </div>
+        </div>
+      )}
+    </SC_BackgroundContainer>
+  );
 };
 
 export default SettingsProfileView;
