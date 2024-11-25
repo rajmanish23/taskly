10024e2ea958ead3c9ede6b64b22d106cd72d93b
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-25 17:30:41 +0530

    Forgot to add inner div alignment container + changed margins of paragraph

--- frontend/src/components/LogoutView/index.tsx
+++ frontend/src/components/LogoutView/index.tsx
@@ -1,12 +1,14 @@
 import Cookies from "js-cookie";
-import { ACCESS_KEY, LOGIN_PAGE_URL, REFRESH_KEY } from "../../constants";
 import { useNavigate } from "react-router-dom";
+
+import { ACCESS_KEY, LOGIN_PAGE_URL, REFRESH_KEY } from "../../constants";
 import {
   SC_BackgroundContainer,
   SC_FormSubmitButton,
-  SC_SettingsViewPara,
+  SC_SettingsViewBackgroundContainer,
 } from "../commonStyles";
 import ViewHeader from "../ViewHeader";
+import { SC_LogoutPara } from "./styles";
 
 const LogoutView = () => {
   const navigate = useNavigate();
@@ -21,12 +23,14 @@ const LogoutView = () => {
   return (
     <SC_BackgroundContainer>
       <ViewHeader h1Text="Do you want to Logout?" />
-      <SC_SettingsViewPara>
-        Please click on the below button to logout of this device.
-      </SC_SettingsViewPara>
-      <SC_FormSubmitButton type="button" onClick={logoutHandler}>
-        Logout
-      </SC_FormSubmitButton>
+      <SC_SettingsViewBackgroundContainer>
+        <SC_LogoutPara>
+          Please click on the below button to logout of this device.
+        </SC_LogoutPara>
+        <SC_FormSubmitButton type="button" onClick={logoutHandler}>
+          Logout
+        </SC_FormSubmitButton>
+      </SC_SettingsViewBackgroundContainer>
     </SC_BackgroundContainer>
   );
 };
---
+++ frontend/src/components/LogoutView/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { SC_SettingsViewPara } from '../commonStyles'
+
+export const SC_LogoutPara = styled(SC_SettingsViewPara)`
+  margin-bottom: 0;
+  font-size: 18px;
+`
