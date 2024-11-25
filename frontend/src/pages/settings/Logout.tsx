dbb0ebad4f18f695b62cc92e78c5d5128eb6236c
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-25 17:29:47 +0530

    Added the logout component to the page

--- frontend/src/pages/settings/Logout.tsx
+++ frontend/src/pages/settings/Logout.tsx
@@ -1,6 +1,7 @@
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import SideBar from "../../components/SideBar";
 import { SC_MainViewContainer } from "../styles";
+import LogoutView from "../../components/LogoutView";
 
 const Logout = () => {
   useDocumentTitle("Logout");
@@ -8,6 +9,7 @@ const Logout = () => {
   return (
     <SC_MainViewContainer>
       <SideBar mode="SETTINGS" selectedView="LOGOUT" selectedTag="" />
+      <LogoutView />
     </SC_MainViewContainer>
   );
 };
