fad10f07dda7582eec1d880428696a771e8996cd
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-25 19:56:12 +0530

    Gave a default value for previous page context so that in the case of an hard reset, the back button could still take back to the home page

--- frontend/src/context.tsx
+++ frontend/src/context.tsx
@@ -9,7 +9,7 @@ export type PageContextType = {
 export const PageContext = React.createContext<PageContextType | null>(null);
 
 const PageContextProvider = ({children}: React.PropsWithChildren) => {
-  const [previousPageState, setPreviousPageState] = useState("")
+  const [previousPageState, setPreviousPageState] = useState("/")
   
   const setPreviousPage = (currentPageVal: string) => {
     setPreviousPageState(currentPageVal);
