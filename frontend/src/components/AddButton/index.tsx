f377b97d1a9866f7175d2fec4d046cd8699525f7
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-26 14:33:37 +0530

    Added 2 modes for the AddEditButton (was AddButton before)

--- frontend/src/components/AddButton/index.tsx
+++ frontend/src/components/AddButton/index.tsx
@@ -1,15 +1,21 @@
 import { AiFillPlusCircle } from "react-icons/ai";
+import { MdEditSquare } from "react-icons/md";
 import { SC_AddTagButton } from "./styles";
 import { STYLE_ICON_MARGINS } from "../../constants";
 
 type AddButtonProp = {
+  mode: "ADD" | "EDIT";
   text: string;
 };
 
-export const AddButton = ({ text }: AddButtonProp) => {
+export const AddEditButton = ({ text, mode }: AddButtonProp) => {
   return (
     <SC_AddTagButton title={text}>
-      <AiFillPlusCircle style={STYLE_ICON_MARGINS} />
+      {mode === "ADD" ? (
+        <AiFillPlusCircle style={STYLE_ICON_MARGINS} />
+      ) : (
+        <MdEditSquare style={STYLE_ICON_MARGINS} />
+      )}
       {text}
     </SC_AddTagButton>
   );
