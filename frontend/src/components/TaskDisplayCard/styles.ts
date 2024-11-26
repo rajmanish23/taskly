d22286e5e82f8a48daf8b4cbe9d5219c22e5b747
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-26 12:33:33 +0530

    Adjusted the width of the task names in the task list item card so that the due date won't wrap weirdly

--- frontend/src/components/TaskDisplayCard/styles.ts
+++ frontend/src/components/TaskDisplayCard/styles.ts
@@ -57,6 +57,7 @@ export const SC_BaseParagraph = styled.p`
 export const SC_TaskNameHeading = styled(SC_BaseParagraph)`
   font-size: 24px;
   font-weight: 600;
+  width: 60%;
 `;
 
 export const SC_DateContainer = styled.div<{
