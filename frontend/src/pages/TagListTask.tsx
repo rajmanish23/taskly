5782c60ac96069b9dbc937100ffb1ae4d37a96fc
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-26 13:28:45 +0530

    formatting + title name change

--- frontend/src/pages/TagListTask.tsx
+++ frontend/src/pages/TagListTask.tsx
@@ -1,13 +1,14 @@
+import { useParams } from "react-router-dom";
+
 import TasksListView from "../components/TasksListView";
 import useDocumentTitle from "../hooks/useDocumentTitle";
-import { useParams } from "react-router-dom";
 import { SC_MainViewContainer } from "./styles";
 import SideBar from "../components/SideBar";
 
 const TagListTask = () => {
-  useDocumentTitle("Tag List task thing");
-
   const { tagId } = useParams();
+
+  useDocumentTitle("Tag");
 
   return (
     <SC_MainViewContainer>
