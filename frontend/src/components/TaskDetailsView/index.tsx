7e902e4136a6bf6723b0fb73df731b2507aaee32
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-11-26 14:36:45 +0530

    Started work on the TaskDetailsView comp

--- frontend/src/components/TaskDetailsView/index.tsx
+++ frontend/src/components/TaskDetailsView/index.tsx
@@ -1,9 +1,16 @@
+import { SC_BackgroundContainer } from "../commonStyles";
+import ViewHeader from "../ViewHeader";
+
 type Props = {
   taskId?: string;
 };
 
 const TaskDetailsView = ({ taskId }: Props) => {
-  return <div>{taskId}</div>;
+  return (
+    <SC_BackgroundContainer>
+      <ViewHeader h1Text="Viewing Task" editButtonText="Edit Task" />
+    </SC_BackgroundContainer>
+  );
 };
 
 export default TaskDetailsView;
