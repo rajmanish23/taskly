798b6d8ea8ac73ab0e0526d55322657568b9a765
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2024-12-04 09:26:40 +0530

    Started blueprinting the form thing. Done the header for now.

--- frontend/src/components/AddEditModalPopup/index.tsx
+++ frontend/src/components/AddEditModalPopup/index.tsx
@@ -1,22 +1,31 @@
 import { useRef, useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { MdEditSquare } from "react-icons/md";
+import { IoCloseCircle } from "react-icons/io5";
+import { FaHashtag } from "react-icons/fa6";
 
 import {
   SC_AddTagButton,
   SC_OverlayBGContainer,
   SC_PopupCloseButton,
   SC_PopupContentContainer,
-  SC_PopupHeader,
-  SC_PopupPara,
 } from "./styles";
 import { STYLE_ICON_MARGINS } from "../../constants";
+import { isTag } from "../../utils/objectTypeCheckers";
+import { SC_TagItemContainer } from "../commonStyles";
+import isColorDark from "../../utils/isColorDark";
 
-type ContentProps = {
+type CommonProps = {
+  mode: "CREATE" | "EDIT";
+  what: "TAG" | "TASK" | "SUBTASK";
+  where?: Tag | Task;
+};
+
+type ContentProps = CommonProps & {
   closeFn: () => void;
 };
 
-const AddEditForm = ({ closeFn }: ContentProps) => {
+const AddEditForm = ({ closeFn, mode, what, where }: ContentProps) => {
   const contentRef = useRef<HTMLDivElement>(null);
 
   const closeOnBgClick = (e: React.MouseEvent) => {
@@ -28,25 +37,62 @@ const AddEditForm = ({ closeFn }: ContentProps) => {
   return (
     <SC_OverlayBGContainer ref={contentRef} onClick={closeOnBgClick}>
       <SC_PopupContentContainer>
-        <SC_PopupHeader>Lorem ipsum dolor sit amet consectetur</SC_PopupHeader>
-        <SC_PopupPara>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus iure
-          autem veniam, unde quasi similique amet optio ipsa corrupti, harum quo
-          enim tempora? Nihil rerum vitae eum odio! Consequatur, rerum.
-        </SC_PopupPara>
-        <br />
-        <SC_PopupCloseButton onClick={closeFn}>Close Modal</SC_PopupCloseButton>
+        <div className="top-header-row">
+          <div className="top-row-left-container">
+            <SC_PopupCloseButton onClick={closeFn}>
+              <IoCloseCircle />
+            </SC_PopupCloseButton>
+            <h1 className="top-header">
+              {mode === "CREATE" ? "Creating" : "Editing"}
+            </h1>
+            <h1 className="top-what-header">
+              {what === "SUBTASK"
+                ? "Sub-Task"
+                : what === "TAG"
+                ? "Tag"
+                : "Task"}
+            </h1>
+            {mode === "CREATE" ? (
+              where === undefined ? (
+                <></>
+              ) : isTag(where) ? (
+                <>
+                  <h1 className="top-header">in</h1>
+                  <SC_TagItemContainer
+                    key={where.sId}
+                    $color={where.colorHex}
+                    $isColorDark={isColorDark(where.colorHex)}
+                  >
+                    <FaHashtag style={STYLE_ICON_MARGINS} />
+                    {where.name}
+                  </SC_TagItemContainer>
+                </>
+              ) : (
+                <>
+                  <h1 className="top-header">in</h1>
+                  <h1 className="top-where-header">{where.name}</h1>
+                </>
+              )
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
       </SC_PopupContentContainer>
     </SC_OverlayBGContainer>
   );
 };
 
-type AddButtonProp = {
-  mode: "ADD" | "EDIT";
+type AddButtonProp = CommonProps & {
   text: string;
 };
 
-export const AddEditModalPopup = ({ text, mode }: AddButtonProp) => {
+export const AddEditModalPopup = ({
+  text,
+  mode,
+  what,
+  where,
+}: AddButtonProp) => {
   const [showModal, setShowModal] = useState(false);
 
   const onClickToggleModal = () => {
@@ -56,14 +102,21 @@ export const AddEditModalPopup = ({ text, mode }: AddButtonProp) => {
   return (
     <>
       <SC_AddTagButton title={text} onClick={onClickToggleModal}>
-        {mode === "ADD" ? (
+        {mode === "CREATE" ? (
           <AiFillPlusCircle style={STYLE_ICON_MARGINS} />
         ) : (
           <MdEditSquare style={STYLE_ICON_MARGINS} />
         )}
         {text}
       </SC_AddTagButton>
-      {showModal && <AddEditForm closeFn={onClickToggleModal} />}
+      {showModal && (
+        <AddEditForm
+          closeFn={onClickToggleModal}
+          mode={mode}
+          what={what}
+          where={where}
+        />
+      )}
     </>
   );
 };
