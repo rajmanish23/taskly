b088324d3a30b6ae9f29569079b3d5f78bf411a4
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2025-01-04 18:31:00 +0530

    Added text and button for the Not found page

--- frontend/src/components/PageNotFoundErrorView/index.tsx
+++ frontend/src/components/PageNotFoundErrorView/index.tsx
@@ -1,7 +1,23 @@
-const PageNotFoundErrorView = () => {
-  return (
-    <div>PageNotFoundErrorView</div>
-  )
-}
+import {
+  areTokensValid,
+  isRefreshTokenExpired,
+} from "../../utils/tokenValidator";
 
-export default PageNotFoundErrorView
+const PageNotFoundErrorView = () => {
+  const isLoggedIn = areTokensValid() && !isRefreshTokenExpired();
+
+  return (
+    <div>
+      <h1>404</h1>
+      <p>
+        The page you were looking for might have been removed, had it's name
+        changed, or is temporarily unavailable
+      </p>
+      <div>
+        {isLoggedIn ? <button>Go to Home</button> : <button>Login</button>}
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFoundErrorView;
