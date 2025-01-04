182b12cb68367df1d04d753922a8be3333c868a5
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2025-01-04 19:16:21 +0530

    Finally completed the error pages for "page not found" and "server unreachable" scenes

--- frontend/src/components/PageNotFoundErrorView/index.tsx
+++ frontend/src/components/PageNotFoundErrorView/index.tsx
@@ -1,31 +1,56 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   areTokensValid,
   isRefreshTokenExpired,
 } from "../../utils/tokenValidator";
-import { SC_BackgroundContainer } from "./styles";
+import {
+  SC_BackgroundContainer,
+  SC_PageErrorImage,
+  SC_PageRedirectButton,
+  SC_ServerErrorImage,
+  SC_SideContainer,
+  SC_SideText,
+} from "./styles";
 import serverUnreachable from "../../assets/server-unreachable.png";
+import { LOGIN_PAGE_URL, TODAY_PAGE_URL } from "../../constants";
 
 type Props = {
   errorMode: "NOT_FOUND" | "SERVER_UNREACHABLE";
 };
 
 const RouteServerErrorView = ({ errorMode }: Props) => {
+  const navigate = useNavigate();
+
   const isLoggedIn = areTokensValid() && !isRefreshTokenExpired();
 
   return (
     <SC_BackgroundContainer>
       {errorMode === "NOT_FOUND" ? (
-        <h1>404</h1>
+        <SC_PageErrorImage>404</SC_PageErrorImage>
       ) : (
-        <img src={serverUnreachable} alt="Server Unreachable" />
+        <SC_ServerErrorImage src={serverUnreachable} alt="Server Unreachable" />
       )}
-      <p>
-        The page you were looking for might have been removed, had it's name
-        changed, or is temporarily unavailable
-      </p>
-      <div>
-        {isLoggedIn ? <button>Go to Home</button> : <button>Login</button>}
-      </div>
+      <SC_SideContainer>
+        <SC_SideText>
+          {errorMode === "NOT_FOUND"
+            ? "The page you were looking for might have been removed, had it's name changed, or is temporarily unavailable"
+            : "It looks like we cannot reach our servers at the moment. Please try again later."}
+        </SC_SideText>
+        {errorMode === "NOT_FOUND" ? (
+          isLoggedIn ? (
+            <SC_PageRedirectButton onClick={() => navigate(TODAY_PAGE_URL)}>
+              Go to Home
+            </SC_PageRedirectButton>
+          ) : (
+            <SC_PageRedirectButton onClick={() => navigate(LOGIN_PAGE_URL)}>
+              Login
+            </SC_PageRedirectButton>
+          )
+        ) : (
+          <></>
+        )}
+      </SC_SideContainer>
     </SC_BackgroundContainer>
   );
 };
--- frontend/src/components/PageNotFoundErrorView/styles.ts
+++ frontend/src/components/PageNotFoundErrorView/styles.ts
@@ -1,9 +1,66 @@
 import styled from "styled-components";
+import {
+  STYLE_ACTIVE_BUTTON_HOVER_HIGHLIGHT_COLOR,
+  STYLE_TRANSITION_TIME,
+} from "../../constants";
 
 export const SC_BackgroundContainer = styled.div`
   flex-grow: 1;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+  padding-left: 20%;
+  padding-right: 20%;
+`;
+
+export const SC_PageErrorImage = styled.h1`
+  font-size: 228px;
+  margin: 0;
+  font-family: "Courier New", Courier, monospace;
+  text-align: center;
+`;
+
+export const SC_ServerErrorImage = styled.img`
+  width: 400px;
+  margin-right: 10px;
+  height: auto;
+`;
+
+export const SC_SideContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 20px;
+  text-align: left;
+`;
+
+export const SC_SideText = styled.p`
+  font-size: 1.5rem;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 20px;
+  text-align: left;
+`;
+
+export const SC_PageRedirectButton = styled.button`
+  &:hover {
+    border: 3px solid ${STYLE_ACTIVE_BUTTON_HOVER_HIGHLIGHT_COLOR};
+  }
+  cursor: pointer;
+  background-color: #8c2d2d;
+  border: 3px solid transparent;
+  border-radius: 10px;
+  padding: 12px;
+  color: white;
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: border ${STYLE_TRANSITION_TIME};
+  margin-bottom: 18px;
 `;
