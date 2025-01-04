967d43abecea2718b65673518861bd0de5c2bf72
Raj Manish <13066821+rajmanish23@users.noreply.github.com>
2025-01-04 18:31:45 +0530

    Removed unnecessary isAuthorized function

--- frontend/src/API/isAuthorizedAPI.ts
+++ frontend/src/API/isAuthorizedAPI.ts
@@ -6,7 +6,6 @@ import {
   REFRESH_KEY,
   TOKEN_REFRESH_API_URL,
 } from "../constants";
-import { jwtDecode, JwtPayload } from "jwt-decode";
 import { decamelizeKeys } from "humps";
 
 export const baseTokenlessAPI = axios.create({
@@ -37,39 +36,3 @@ export const refreshSession = async () => {
     throw new Error("API threw some other non-error status code");
   }
 };
-
-const isAuthorized = async () => {
-  const accessToken = Cookies.get(ACCESS_KEY);
-  const refreshToken = Cookies.get(REFRESH_KEY);
-  if (accessToken === undefined || refreshToken === undefined) {
-    return false;
-  }
-  try {
-    const decodedAccessToken = jwtDecode<JwtPayload>(accessToken);
-    const decodedRefreshToken = jwtDecode<JwtPayload>(refreshToken);
-    const accessTokenExpiration = decodedAccessToken.exp; // this will be in seconds
-    const refreshTokenExpiration = decodedRefreshToken.exp;
-    const now = Date.now() / 1000; // this ensures the "now" is in seconds, not milliseconds
-    if (
-      accessTokenExpiration === undefined ||
-      refreshTokenExpiration === undefined
-    ) {
-      return false;
-    }
-    if (accessTokenExpiration > now) {
-      // if current expiration is greater than the current time so essentially if it's not expired
-      // then we can return true regardless cuz the user's access token works
-      return true;
-    }
-    if (refreshTokenExpiration < now) {
-      // if the refresh token is expired
-      return false;
-    }
-    return await refreshSession();
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-};
-
-export default isAuthorized;
